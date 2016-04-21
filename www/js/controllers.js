9c337dd53d0ab92de3796f2ee2a1081893741931
starcwl <starcwl@gmail.com>
2016-04-21 12:33:11 +0800

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('page2Ctrl', function($scope) {
+
+})
+   
+.controller('page3Ctrl', function($scope) {
+
+})
+   
+.controller('page4Ctrl', function($scope) {
+
+})
+         
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+ 
