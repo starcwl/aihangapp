94b72ce142092d018d3def3dc0c1551cb77f5969
starcwl <starcwl@gmail.com>
2016-04-21 16:04:09 +0800

    add map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,23 @@
 angular.module('app.controllers', [])
-  
+
 .controller('page2Ctrl', function($scope) {
 
 })
-   
-.controller('page3Ctrl', function($scope) {
 
+.controller('page3Ctrl', function($scope) {
+    var map = new BMap.Map("ditu"); // 创建地图实例  
+    var point = new BMap.Point(116.404, 39.915); // 创建点坐标  
+    map.centerAndZoom(point, 15); // 初始化地图，设置中心点坐标和地图级别
 })
-   
+
 .controller('page4Ctrl', function($scope) {
 
 })
-         
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
- 
