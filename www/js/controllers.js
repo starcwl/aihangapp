71680dca0dd1194d90d18c202fe14710870e794e
starcwl <starcwl@gmail.com>
2016-04-26 15:21:19 +0800

    fix geo for baidu bias

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,13 +4,44 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('locationCtrl', function($scope, $window) {
-    var map = new BMap.Map("ditu"); // 创建地图实例  
-    var point = new BMap.Point(116.404, 39.915); // 创建点坐标  
-    map.centerAndZoom(point, 15); // 初始化地图，设置中心点坐标和地图级别
+.controller('locationCtrl', function($scope, $window, $http, GeoService) {
 
-    var marker = new BMap.Marker(point); // 创建标注    
-    map.addOverlay(marker); // 将标注添加到地图中
+
+
+    var map = new BMap.Map("ditu"); // 创建地图实例  
+    var selfPoint = new BMap.Point(116.404, 39.915); // 初始化点坐标在首都  
+    map.centerAndZoom(selfPoint, 15); // 初始化地图，设置中心点坐标和地图级别
+    var selfMarker = new BMap.Marker(selfPoint);
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+      alert('dd')
+            GeoService.ConvertGeo({
+                longitude: position.coords.longitude,
+                latitude: position.coords.latitude,
+            }, function(success, response) {
+                if (success) {
+                    selfPoint = new BMap.Point(response.longitude, response.latitude);
+                    map.centerAndZoom(selfPoint, 15); // 初始化地图，设置中心点坐标和地图级别
+                    map.removeOverlay(selfMarker);
+                    selfMarker = new BMap.Marker(selfPoint); // 创建标注    
+                    map.addOverlay(selfMarker); // 将标注添加到地图中
+                    console.log('response is :', response);
+                }
+            });
+        },
+        // Optional settings below
+        function(error) {
+
+            console.log('code: ' + error.code + '\n' +
+                'message: ' + error.message + '\n');
+        }, {
+            timeout: 1000,
+            enableHighAccuracy: true,
+            maximumAge: Infinity
+        }
+    );
+
 
     var zoomControl = new BMap.ZoomControl();
     map.addControl(zoomControl); //添加缩放控件  
@@ -19,19 +50,8 @@ angular.module('app.controllers', [])
     map.addControl(scaleControl); //添加比例尺控件  
 
 
-    var options = {
-        center: point,
-        bgColor: '#00abc4',
-        logo: 'img/marker.png',
-        price: '189',
-        info: 'hello'
-    };
 
-    var hotelMark = new HotelMarkOverlay(options);
-    map.addOverlay(hotelMark);
-    hotelMark.addEventListener('click', function(e) {
-        console.log(e);
-    });
+
 
 
     function addMarker(point, index) { // 创建图标对象     
@@ -60,14 +80,37 @@ angular.module('app.controllers', [])
         var point = new BMap.Point(bounds.getSouthWest().lng + lngSpan * (Math.random() * 0.7 + 0.15), bounds.getSouthWest().lat + latSpan * (Math.random() * 0.7 + 0.15));
         addMarker(point, i);
     }
-    map.addEventListener('zoomend', function(e) {
-        map.clearOverlays();
 
-        for (var i = 0; i < 3; i++) {
-            var point = new BMap.Point(bounds.getSouthWest().lng + lngSpan * (Math.random() * 0.7 + 0.15), bounds.getSouthWest().lat + latSpan * (Math.random() * 0.7 + 0.15));
-            addMarker(point, i);
-        }
-    });
+    //zoomEnd event
+    // map.addEventListener('zoomend', function(e) {
+    //     map.clearOverlays();
+
+    //     for (var i = 0; i < 3; i++) {
+    //         var point = new BMap.Point(bounds.getSouthWest().lng + lngSpan * (Math.random() * 0.7 + 0.15), bounds.getSouthWest().lat + latSpan * (Math.random() * 0.7 + 0.15));
+    //         addMarker(point, i);
+    //     }
+    // });
+
+
+
+    function onSuccess(position) {
+        selfPoint = new BMap.Point(position.coords.longitude, position.coords.latitude);
+        map.removeOverlay(selfMarker);
+        selfMarker = new BMap.Marker(selfPoint); // 创建标注    
+        map.addOverlay(marker); // 将标注添加到地图中
+        console.log('updated position:', position);
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        console.log('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    // Options: throw an error if no update is received every 30 seconds.
+    //
+    // navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 3000 });
 
 })
 
